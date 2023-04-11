f42c092302f5cc119db00601159dcb5d64837960
antonio202 <46777518+antonio202@users.noreply.github.com>
2023-04-11 19:47:12 +0200

    CRUD Usuarios

--- Laravel/project-3-2/app/Http/Controllers/UsersController.php
+++ Laravel/project-3-2/app/Http/Controllers/UsersController.php
@@ -6,5 +6,59 @@ use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
-    //
+    public function crear(Request $request){
+        $request->validate(['name'=>'required','apellidos'=>'required','email'=>'required','password'=>'required','telefono'=>'required']);
+        $yaRegistrado = User::where('email',$request->email)->get();
+        if($yaRegistrado->count()>0){
+            return back() -> with('mensaje', 'El usuario ya existe');
+        }else{
+            $userNuevo = new User;
+            $userNuevo->name = $request->name;
+            $userNuevo->apellidos = $request->apellidos;
+            $userNuevo->email = $request->email;
+            $userNuevo->password = $request->password;
+            $userNuevo->telefono = $request->telefono;
+            $userNuevo->rol = 'cliente';
+            $userNuevo->save();
+            $carritoNuevo = new Carrito;
+            $carritoNuevo->user_id = $userNuevo->id;
+            $carritoNuevo->save();
+            return back() -> with('mensaje', 'El usuario se ha creado correctamente');
+        }   
+    }
+
+    public function eliminar($id){
+        $userEliminar = User::findOrFail($id);
+        $carritoEliminar = Carrito::where('user_id',$userEliminar->id)->get();
+        $carritoEliminar[0]->delete();
+        $userEliminar->delete();
+        return back() -> with('mensaje', 'Persona Eliminada');
+    }
+
+    public function listar(){
+        $usuarios = User::all();
+        return view('index', ['usuarios'=>$usuarios]);
+    }
+
+    public function visualizar($id){
+        $user=User::findOrFail($id);
+        return view('visualizar',['usuario'=>$user]);
+    }
+
+    public function editar($id){
+        $user=User::findOrFail($id);
+        return view('editar',['usuario'=>$user]);
+    }
+
+    public function actualizar(Request $request, $id){
+        $request->validate(['name' => ['required', 'string', 'max:255'],
+        'apellidos' => ['required', 'string', 'max:255'],
+        'telefono' => ['required', 'integer']]);
+        $user=User::findOrFail($id);
+        $user->name = $request->name;
+        $user->apellidos = $request->apellidos;
+        $user->telefono = $request->telefono;
+        $user->save();
+        return view('welcome');
+    }
 }
--- Laravel/project-3-2/routes/web.php
+++ Laravel/project-3-2/routes/web.php
@@ -22,7 +22,6 @@ Route::get('/home', function () {
 })->middleware('auth','verified');
 
 Route::post('/crear','App\Http\Controllers\UsersController@crear')->name('usuarios.crear');
-Route::post('/login','App\Http\Controllers\UsersController@login')->name('usuarios.login');
 Route::delete('personas/{id}','App\Http\Controllers\UsersController@eliminar')->name('usuarios.eliminar');
 Route::get('editar/{id}','App\Http\Controllers\UsersController@editar')->name('usuarios.editar');
 Route::put('editar/{id}','App\Http\Controllers\UsersController@actualizar')->name('usuarios.actualizar');
