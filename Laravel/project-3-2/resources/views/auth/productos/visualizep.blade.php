7af50bc1fa19b285a169b74a716c29a2470bdf31
Pablo-Hidalgo-Prados <pablohidalgo3@gmail.com>
2023-04-14 19:36:06 +0200

    Update visualizep.blade.php

--- Laravel/project-3-2/resources/views/auth/productos/visualizep.blade.php
+++ Laravel/project-3-2/resources/views/auth/productos/visualizep.blade.php
@@ -2,19 +2,17 @@
 
 @section('content')
 <div class="w-50 border rounded p-3 mx-auto">
-    <h3 class="text-center">Visualizando a {{ $user->name }}</h3>
+    <h3 class="text-center">Visualizando {{ $producto->nombre }}</h3>
     <form action="{{ route('usuarios.volver') }}" method="post">
         @csrf
         <label>Nombre:</label>
-        <input class="form-control mb-3" type="text" disabled value="{{ $user->name }}" name="name">
-        <label>Apellidos:</label>
-        <input class="form-control mb-3" type="text" disabled value="{{ $user->apellidos }}" name="apellidos">
-        <label>Email:</label>
-        <input class="form-control mb-3" type="email" disabled value="{{ $user->email }}" name="email">
-        <label>Rol:</label>
-        <input class="form-control mb-3" type="text" disabled value="{{ $user->rol }}" name="rol">
-        <label>Teléfono:</label>
-        <input class="form-control mb-3" type="number" disabled value="{{ $user->telefono }}" name="telefono">
+        <input class="form-control mb-3" type="text" disabled value="{{ $producto->nombre }}" name="nombre">
+        <label>Descripción:</label>
+        <input class="form-control mb-3" type="text" disabled value="{{ $producto->descripcion }}" name="descripcion">
+        <label>Precio:</label>
+        <input class="form-control mb-3" type="email" disabled value="{{ $producto->precio }}" name="precio">
+        <label>Stock:</label>
+        <input class="form-control mb-3" type="text" disabled value="{{ $producto->stock }}" name="stock">
         <button class="btn btn-success" type="submit">Volver</button>
     </form>
 
