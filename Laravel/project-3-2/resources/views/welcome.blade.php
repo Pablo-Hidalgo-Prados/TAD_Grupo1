d17b85259ecbdc3803b9084c2b86354b5ba64918
Pablo-Hidalgo-Prados <pablohidalgo3@gmail.com>
2023-04-18 17:09:11 +0200

    Update carousel

--- Laravel/project-3-2/resources/views/welcome.blade.php
+++ Laravel/project-3-2/resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <div class="m-lg-5">
             <div class="m-lg-5">
                 <div class="m-lg-5">
-                    <div id="carouselExampleCaptions" class="carousel slide m-5 rounded-4" data-bs-ride="false">
+                    <div id="carouselExampleCaptions" class="carousel carousel-dark slide m-5 rounded-4" data-bs-ride="false">
                         <div class="carousel-indicators">
                             <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0"
                                 class="active" aria-current="true" aria-label="Slide 1"></button>
