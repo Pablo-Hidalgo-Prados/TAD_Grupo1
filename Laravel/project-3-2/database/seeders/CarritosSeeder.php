efb35526e1e2be9fc29610ac97450e9edf1b87e2
antonio202 <46777518+antonio202@users.noreply.github.com>
2023-04-24 18:05:18 +0200

    Database seeders y seeder para carrito

---
+++ Laravel/project-3-2/database/seeders/CarritosSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Carrito;
+
+class CarritosSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+
+     protected $depends = [UsersSeeder::class];
+
+    public function run()
+    {
+        Carrito::create([
+            'user_id' => '2',
+            'total' => '0'
+        ]);
+    }
+}
