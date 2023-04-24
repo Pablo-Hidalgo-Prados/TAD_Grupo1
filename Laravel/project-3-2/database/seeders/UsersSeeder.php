0314b079b09d4658599ad390acb865872cd53a7b
antonio202 <46777518+antonio202@users.noreply.github.com>
2023-04-24 17:44:45 +0200

    Más seeder y TODO actualizado

--- Laravel/project-3-2/database/seeders/UsersSeeder.php
+++ Laravel/project-3-2/database/seeders/UsersSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use Illuminate\Support\Carbon;
 
 class UsersSeeder extends Seeder
 {
@@ -21,7 +22,18 @@ class UsersSeeder extends Seeder
             'email' => 'pablo@pablo.com',
             'password' => bcrypt('12345678'),
             'rol' => 'admin',
-            'telefono' => '652154785'
+            'telefono' => '652154785',
+            'email_verified_at' => Carbon::now()
+        ]);
+
+        User::create([
+            'name' => 'Pepe',
+            'apellidos' => 'Pepe Pepe',
+            'email' => 'pepe@pepe.com',
+            'password' => bcrypt('12345678'),
+            'rol' => 'cliente',
+            'telefono' => '639854752',
+            'email_verified_at' => Carbon::now()
         ]);
     }
 }
