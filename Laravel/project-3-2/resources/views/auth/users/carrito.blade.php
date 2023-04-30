d45994452ffca81789c3affb163b33b1e6c45395
Pablo-Hidalgo-Prados <pablohidalgo3@gmail.com>
2023-05-01 01:50:11 +0200

    Foto de perfil corregida + carrito + layout

--- Laravel/project-3-2/resources/views/auth/users/carrito.blade.php
+++ Laravel/project-3-2/resources/views/auth/users/carrito.blade.php
@@ -3,21 +3,25 @@
 @section('content')
 <div class="m-4">
     <h3 class="text-center">Carrito de {{ $user->name }}</h3>
+    <div class="d-flex justify-content-center mt-4 mb-4 me-4">
+            <form action="{{ route('carritos.vaciar') }}" method="POST">
+                @csrf
+                <button class="btn btn-success ml-2" type="submit">Vaciar carrito</button>
+            </form>
+        </div>
     <table class="table border-success text-black text-center w-75 mx-auto mt-5">
-        <th>ID</th>
         <th>Nombre</th>
-        <th class="d-none d-lg-block">Descripción</th>
+        <th class="d-none d-lg-grid">Descripción</th>
         <th>Precio</th>
         <th>Cantidad</th>
         <th>Acciones</th>
         @foreach ($productos_carrito as $producto)
         <tr>
-            <td>{{ $producto->id }}</td>
-            <td>{{ $producto->nombre }}</td>
-            <td class="d-none d-lg-block">{{ $producto->descripcion }}</td>
-            <td>{{ $producto->precio }}</td>
-            <td>{{ $producto->pivot->cantidad }}</td>
-            <td class="d-flex xs:block justify-content-center">
+            <td class="align-middle">{{ $producto->nombre }}</td>
+            <td class="d-none d-lg-table-cell align-middle">{{ $producto->descripcion }}</td>
+            <td class="align-middle">{{ $producto->precio }}</td>
+            <td class="align-middle">{{ $producto->pivot->cantidad }}</td>
+            <td class="d-flex xs:block justify-content-center align-middle">
                 <form action="{{ route('carritos.reducir') }}" method="post">
                     @csrf
                     <input type="hidden" name="producto_id" value="{{ $producto->id }}">
@@ -33,18 +37,8 @@
         @endforeach
     </table>
     <h3 class="text-center">Precio total {{ $precio_total }}</h3>
-    <div class="d-flex flex-grow-1 mx-4">
-        <div class="d-flex justify-content-left mt-4 mb-4 me-4">
-            <form action="{{ route('usuarios.volver') }}" method="POST">
-                @csrf
-                <button class="btn btn-success me-2" type="submit">Volver</button>
-            </form>
-            <form action="{{ route('carritos.vaciar') }}" method="POST">
-                @csrf
-                <button class="btn btn-success ml-2" type="submit">Vaciar carrito</button>
-            </form>
-        </div>
-        <div class="d-flex justify-content-end mt-4 mb-4 ml-4 flex-grow-1">
+    <div class="d-flex justify-content-center"> 
+        <div class="mt-4 mb-4 ml-4">
             <form action="{{ route('compras.crear') }}" method="POST">
                 @csrf
                 <!-- Agrega los campos del formulario que contienen los datos necesarios -->
@@ -60,10 +54,15 @@
                 <label>¿Tiene algún código?</label>
                 <input type="text" name="codigo_descuento" class="form-control mb-3">
                 @if($direcciones->count()==0)
+                <div class="d-flex justify-content-center">
                 <button class="btn btn-success mt-2" disabled type="submit">Comprar</button>
+                </div>
                 @else
+                <div class="d-flex justify-content-center">
                 <button class="btn btn-success" type="submit">Comprar</button>
+                </div>
                 @endif
+                
             </form>
         </div>
     </div>
--- Laravel/project-3-2/resources/views/layout.blade.php
+++ Laravel/project-3-2/resources/views/layout.blade.php
@@ -31,7 +31,33 @@
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <div class="d-flex flex-grow-1">
-                    <ul class="nav flex-grow-1">
+
+                    <ul class="nav flex-grow-1 list-unstyled d-block d-md-none">
+                        <li class="nav-item">
+                            <a class="nav-link fw-bold text-light" href="/">Inicio</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link fw-bold text-light" href="{{ route('rutas') }}">Rutas</a>
+                        </li>
+
+                        @if(isset(Auth::user()->id))
+                        @if(Auth::user()->rol=='admin')
+                        <li class="nav-item">
+                            <a class="nav-link fw-bold text-light" href="/panel">Panel Admin</a>
+                        </li>
+                        @else
+                        <li class="nav-item">
+                            <a class="nav-link fw-bold text-light" href="{{ route('productos.vista') }}">Productos</a>
+                        </li>
+                        @endif
+                        @else
+                        <li class="nav-item">
+                            <a class="nav-link fw-bold text-light" href="{{ route('productos.vista') }}">Productos</a>
+                        </li>
+                        @endif
+                    </ul>
+
+                    <ul class="nav flex-grow-1 d-none d-md-flex">
                         <li class="nav-item">
                             <a class="nav-link fw-bold text-light" href="/">Inicio</a>
                         </li>
@@ -56,13 +82,13 @@
                         @endif
                     </ul>
                     @if (Route::has('login'))
-                    <div class="hidden top-0 me-xl-4 d-sm-flex d-md-flex d-lg-flex d-xxl-flex align-items-right xs:block float-end justify-content-end">
+                    <div class="hidden top-0 me-xl-4 d-sm-block d-md-flex d-lg-flex d-xxl-flex align-items-right xs:block float-end justify-content-end">
                         @auth
                         @if(Auth::user()->rol=='cliente')
                         <form action="{{ route('carritos.visualizar') }}" method="post">
                             @csrf
-                            <button type="submit" class="btn btncarrito text-white rounded-1 d-flex p-2 me-2 ml-2 mb-1 mb-sm-0 mb-md-0 mb-lg-0 mb-xl-0 align-items-center">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="me-1 bi bi-cart4" viewBox="0 0 16 16">
+                            <button type="submit" class="btn btncarrito text-white rounded-1 d-flex p-2 me-2 ml-2 mb-1 mb-sm-1 mb-md-0 mb-lg-0 mb-xl-0 align-items-center" data-bs-toggle="tooltip" data-bs-title="Ver carrito">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart4" viewBox="0 0 16 16">
                                     <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z" />
                                 </svg>
                             </button>
@@ -70,27 +96,27 @@
                         @endif
                         <form action="{{ route('logout') }}" method="post">
                             @csrf
-                            <button type="submit" class="btn btncarrito text-white rounded-1 d-flex p-2 me-2 ml-2 mb-1 mb-sm-0 mb-md-0 mb-lg-0 mb-xl-0 align-items-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
-                                <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"/>
-                                <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
-                            </svg>
+                            <button type="submit" class="btn btncarrito text-white rounded-1 d-flex p-2 me-2 ml-2 mb-1 mb-sm-1 mb-md-0 mb-lg-0 mb-xl-0 align-items-center" data-bs-toggle="tooltip" data-bs-title="Logout">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z" />
+                                    <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z" />
+                                </svg>
                             </button>
                         </form>
                         <form action="{{ route('usuarios.visualizar', Auth::user()->id) }}" method="get">
                             @csrf
-                            <button type="submit" class="btn btncarrito text-white rounded-1 d-flex p-2 me-2 ml-2 mb-1 mb-sm-0 mb-md-0 mb-lg-0 mb-xl-0 align-items-center">
+                            <button type="submit" class="btn btncarrito text-white rounded-1 d-flex p-2 me-2 ml-2 mb-1 mb-sm-1 mb-md-0 mb-lg-0 mb-xl-0 align-items-center" data-bs-toggle="tooltip" data-bs-title="Perfil">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
-                                    <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z"/>
+                                    <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z" />
                                 </svg>
                             </button>
                         </form>
                         @else
-                        <div class="bglogin rounded-4 d-flex p-2 me-2 mb-1 mb-sm-0 mb-md-0 mb-lg-0 mb-xl-0 ">
+                        <div class="bglogin rounded-4 p-2 me-0 me-md-2 me-lg-2 mb-1 mb-sm-1 mb-md-0 mb-lg-0 mb-xl-0 text-center">
                             <a href="{{ route('login') }}" class="text-sm text-light text-decoration-none">Login</a>
                         </div>
                         @if (Route::has('register'))
-                        <div class="bglogin rounded-4 d-flex p-2 ml-2 align-items-center">
+                        <div class="bglogin rounded-4 p-2 ml-2 mb-1 mb-sm-1 mb-md-0 mb-lg-0 mb-xl-0 align-items-center">
                             <a href="{{ route('register') }}" class="text-sm text-light text-decoration-none">Registrarse</a>
                         </div>
                         @endif
@@ -164,6 +190,14 @@
             }
         }
     </script> -->
+
+    <script>
+        $(document).ready(function() {
+            $("body").tooltip({
+                selector: '[data-bs-toggle=tooltip]'
+            });
+        });
+    </script>
 </body>
 
 </html>
