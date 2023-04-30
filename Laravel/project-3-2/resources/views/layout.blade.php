b0831fcbad220ea642a6faee2a7d24ae079a6996
antonio202 <46777518+antonio202@users.noreply.github.com>
2023-04-30 16:48:09 +0200

    Iconos principales

--- Laravel/project-3-2/resources/views/layout.blade.php
+++ Laravel/project-3-2/resources/views/layout.blade.php
@@ -61,23 +61,30 @@
                         @if(Auth::user()->rol=='cliente')
                         <form action="{{ route('carritos.visualizar') }}" method="post">
                             @csrf
-                            <button type="submit" class="btn btncarrito text-white rounded-4 d-flex p-2 me-3 ml-2 mb-1 mb-sm-0 mb-md-0 mb-lg-0 mb-xl-0 align-items-center">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="me-1 bi bi-cart4" viewBox="0 0 16 16">
+                            <button type="submit" class="btn btncarrito text-white rounded-1 d-flex p-2 me-2 ml-2 mb-1 mb-sm-0 mb-md-0 mb-lg-0 mb-xl-0 align-items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="me-1 bi bi-cart4" viewBox="0 0 16 16">
                                     <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z" />
                                 </svg>
-                                Ver Carrito
                             </button>
                         </form>
                         @endif
-                        <div class="bglogin rounded-4 d-flex p-2 me-2 ml-4 mb-1 mb-sm-0 mb-md-0 mb-lg-0 mb-xl-0 align-items-center">
-                            <a class="text-sm text-light text-decoration-none" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
-                        </div>
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        <form action="{{ route('logout') }}" method="post">
                             @csrf
+                            <button type="submit" class="btn btncarrito text-white rounded-1 d-flex p-2 me-2 ml-2 mb-1 mb-sm-0 mb-md-0 mb-lg-0 mb-xl-0 align-items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"/>
+                                <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
+                            </svg>
+                            </button>
                         </form>
-                        <div class="bglogin rounded-4 d-flex p-2 ml-2 align-items-center">
-                            <a href="{{ route('usuarios.visualizar', Auth::user()->id) }}" class="text-sm text-light text-decoration-none">Perfil</a>
-                        </div>
+                        <form action="{{ route('usuarios.visualizar', Auth::user()->id) }}" method="get">
+                            @csrf
+                            <button type="submit" class="btn btncarrito text-white rounded-1 d-flex p-2 me-2 ml-2 mb-1 mb-sm-0 mb-md-0 mb-lg-0 mb-xl-0 align-items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+                                    <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z"/>
+                                </svg>
+                            </button>
+                        </form>
                         @else
                         <div class="bglogin rounded-4 d-flex p-2 me-2 mb-1 mb-sm-0 mb-md-0 mb-lg-0 mb-xl-0 ">
                             <a href="{{ route('login') }}" class="text-sm text-light text-decoration-none">Login</a>
