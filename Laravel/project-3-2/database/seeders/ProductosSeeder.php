fa078961e9c2badf7eea828beb8a6d6c463dd280
antonio202 <46777518+antonio202@users.noreply.github.com>
2023-05-02 13:21:48 +0200

    Seeders productos

--- Laravel/project-3-2/database/seeders/ProductosSeeder.php
+++ Laravel/project-3-2/database/seeders/ProductosSeeder.php
@@ -24,11 +24,75 @@ class ProductosSeeder extends Seeder
         ]);
 
         Producto::create([
+            'nombre' => 'Mochila adidas',
+            'descripcion' => 'Mochila pequeña',
+            'precio' => '10',
+            'stock' => '10',
+            'imagen' => 'images/Mochila_1682433462.jpg'
+        ]);
+
+        Producto::create([
             'nombre' => 'Bastones',
             'descripcion' => 'Bastones de trekking muy PROs',
             'precio' => '15',
             'stock' => '20',
             'imagen' => 'images/Bastones_1682433462.jpg'
+        ]);
+
+        Producto::create([
+            'nombre' => 'Cuerda',
+            'descripcion' => 'Cuerda 20m',
+            'precio' => '30',
+            'stock' => '15',
+            'imagen' => 'images/Cuerda_1682433463.jpg'
+        ]);
+
+        Producto::create([
+            'nombre' => 'Camiseta trekking',
+            'descripcion' => 'Camiseta para senderismo',
+            'precio' => '15',
+            'stock' => '10',
+            'imagen' => 'images/Camiseta_1682433464.jpg'
+        ]);
+
+        Producto::create([
+            'nombre' => 'Cantimplora',
+            'descripcion' => 'Cantimplora para senderismo',
+            'precio' => '20',
+            'stock' => '10',
+            'imagen' => 'images/Cantimplora_1682433465.jpg'
+        ]);
+
+        Producto::create([
+            'nombre' => 'Pedernal',
+            'descripcion' => 'Pedernal supervivencia',
+            'precio' => '20',
+            'stock' => '10',
+            'imagen' => 'images/Pedernal_1682433466.jpg'
+        ]);
+
+        Producto::create([
+            'nombre' => 'Botiquín',
+            'descripcion' => 'Botiquín emergencia',
+            'precio' => '50',
+            'stock' => '10',
+            'imagen' => 'images/Botiquín_1682433467.jpg'
+        ]);
+
+        Producto::create([
+            'nombre' => 'Botas',
+            'descripcion' => 'Botas senderismo nieve',
+            'precio' => '100',
+            'stock' => '10',
+            'imagen' => 'images/Botas_1682433468.jpg'
+        ]);
+
+        Producto::create([
+            'nombre' => 'Guantes',
+            'descripcion' => 'Guantes nieve',
+            'precio' => '40',
+            'stock' => '5',
+            'imagen' => 'images/Guantes_1682433469.jpg'
         ]);
     }
 }
