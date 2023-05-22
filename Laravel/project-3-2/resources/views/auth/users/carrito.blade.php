1636d3a13248f4e745cfd4f2eb01864745e0c485
antonio202 <46777518+antonio202@users.noreply.github.com>
2023-05-22 20:10:30 +0200

    Carrito fallo modificado y mochila

--- Laravel/project-3-2/resources/views/auth/users/carrito.blade.php
+++ Laravel/project-3-2/resources/views/auth/users/carrito.blade.php
@@ -88,17 +88,17 @@
                 <input type="hidden" name="user_id" value="{{ $user->id }}">
                 <input type="hidden" name="agregar" value="carrito">
                 <label>@lang('messages.cart_info_13')</label>
-                <input class="form-control mb-3 input-small" type="text" name="calle">
+                <input class="form-control mb-3 @error('calle') is-invalid @enderror" type="text" name="calle">
                 <label>@lang('messages.cart_info_14')</label>
-                <input class="form-control mb-3 input-small" type="text" name="ciudad">
+                <input class="form-control mb-3 @error('ciudad') is-invalid @enderror" type="text" name="ciudad">
                 <label>@lang('messages.cart_info_15')</label>
-                <input class="form-control mb-3 input-small" type="text" name="codigo_postal">
+                <input class="form-control mb-3 @error('codigo_postal') is-invalid @enderror" type="text" name="codigo_postal">
                 <label>@lang('messages.cart_info_16')</label>
-                <input class="form-control mb-3 input-small" type="number" name="numero">
+                <input class="form-control mb-3 @error('numero') is-invalid @enderror" type="number" name="numero">
                 <label>@lang('messages.cart_info_17')</label>
-                <input class="form-control mb-3 input-small" type="number" name="planta">
+                <input class="form-control mb-3" type="number" name="planta">
                 <label>@lang('messages.cart_info_18')</label>
-                <input class="form-control mb-3 input-small" type="text" name="puerta">
+                <input class="form-control mb-3" type="text" name="puerta">
                 <button class="btn btn-success" type="submit">@lang('messages.cart_info_19')</button>
             </form>
         </div>
