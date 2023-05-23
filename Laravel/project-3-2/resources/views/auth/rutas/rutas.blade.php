c4c32a983d8c1a0c2838796fcbb7dbc45b407b31
antonio202 <46777518+antonio202@users.noreply.github.com>
2023-05-23 20:08:26 +0200

    Rutas con mensaje

--- Laravel/project-3-2/resources/views/auth/rutas/rutas.blade.php
+++ Laravel/project-3-2/resources/views/auth/rutas/rutas.blade.php
@@ -2,7 +2,14 @@
 
 @section('content')
 <html lang="{{ app()->getLocale() }}">
-
+<script>
+        function showAlertAndOpenNewTab(url) {
+        if (confirm('Se está abriendo un enlace a otra página para descargar la ruta. ¿Desea continuar?')) {
+            var newTab = window.open(url, '_blank');
+            newTab.focus();
+        }
+    }
+</script>
 <section>
     <div class="text-center my-3">
         <h1 class="strong">@lang('messages.trails_info_1')</h1>
@@ -10,26 +17,26 @@
     </div>
     <div class="container text-center my-3">
         <div class="row row-cols-2">
-            <img src="https://s0.wklcdn.com/image_10/321744/16921732/10636182Master.jpg" onclick="window.location.href='https://es.wikiloc.com/rutas-senderismo/6-picos-s-calderona-pico-del-aguila-el-morro-la-furiona-xocainet-la-redona-el-rodeno-de-la-cruz-y-c-16921732'">
-            <img src="https://s0.wklcdn.com/image_33/1008688/21677560/13689090Master.jpg" onclick="window.location.href='https://es.wikiloc.com/rutas-senderismo/tejos-milenarios-y-lagunillas-del-valle-de-la-angostura-circular-desde-la-isla-inmediaciones-del-p-21677560'">
-            <img src="https://s1.wklcdn.com/image_29/877835/9042917/5352688Master.jpg" class="my-3" onclick="window.location.href='https://es.wikiloc.com/rutas-senderismo/mirador-de-los-poetas-reloj-de-cela-ducha-de-los-alemanes-las-dehesas-de-cercedilla-9042913'">
-            <img src="https://s2.wklcdn.com/image_30/903981/10595997/6394415Master.jpg" class="my-3" onclick="window.location.href='https://es.wikiloc.com/rutas-senderismo/desfiladero-de-las-xanas-principado-de-asturias-10595997'">
+            <img src="https://s0.wklcdn.com/image_10/321744/16921732/10636182Master.jpg" onclick="showAlertAndOpenNewTab('https://es.wikiloc.com/rutas-senderismo/6-picos-s-calderona-pico-del-aguila-el-morro-la-furiona-xocainet-la-redona-el-rodeno-de-la-cruz-y-c-16921732')">
+            <img src="https://s0.wklcdn.com/image_33/1008688/21677560/13689090Master.jpg" onclick="showAlertAndOpenNewTab('https://es.wikiloc.com/rutas-senderismo/tejos-milenarios-y-lagunillas-del-valle-de-la-angostura-circular-desde-la-isla-inmediaciones-del-p-21677560')">
+            <img src="https://s1.wklcdn.com/image_29/877835/9042917/5352688Master.jpg" class="my-3" onclick="showAlertAndOpenNewTab('https://es.wikiloc.com/rutas-senderismo/mirador-de-los-poetas-reloj-de-cela-ducha-de-los-alemanes-las-dehesas-de-cercedilla-9042913')">
+            <img src="https://s2.wklcdn.com/image_30/903981/10595997/6394415Master.jpg" class="my-3" onclick="showAlertAndOpenNewTab('https://es.wikiloc.com/rutas-senderismo/desfiladero-de-las-xanas-principado-de-asturias-10595997')">
         </div>
     </div>
     <div class="container text-center my-3">
         <div class="row row-cols-2">
-            <img src="https://s0.wklcdn.com/image_10/321744/16820572/10564116Master.jpg" onclick="window.location.href='https://es.wikiloc.com/rutas-senderismo/cruz-del-sierro-desde-la-font-del-marionet-16820565'">
-            <img src="https://s0.wklcdn.com/image_33/1008688/16147018/10114599Master.jpg" onclick="window.location.href='https://es.wikiloc.com/rutas-senderismo/cruz-del-mierlo-sierra-de-los-porrones-la-pedriza-desde-parking-principal-p-n-de-guadarrama-16147016'">
-            <img src="https://s2.wklcdn.com/image_34/1027315/23183804/14727692Master.jpg" class="my-3" onclick="window.location.href='https://es.wikiloc.com/rutas-senderismo/ruta-del-agua-de-bunol-cueva-del-turche-charco-de-las-palomas-charco-manan-cueva-de-las-palomas-y-t-23183804'">
-            <img src="https://s1.wklcdn.com/image_87/2628884/24930448/15907162Master.jpg" class="my-3" onclick="window.location.href='https://es.wikiloc.com/rutas-senderismo/ruta-de-las-tres-cascadas-ardones-clotet-mascarada-cerler-24930446'">
+            <img src="https://s0.wklcdn.com/image_10/321744/16820572/10564116Master.jpg" onclick="showAlertAndOpenNewTab('https://es.wikiloc.com/rutas-senderismo/cruz-del-sierro-desde-la-font-del-marionet-16820565')">
+            <img src="https://s0.wklcdn.com/image_33/1008688/16147018/10114599Master.jpg" onclick="showAlertAndOpenNewTab('https://es.wikiloc.com/rutas-senderismo/cruz-del-mierlo-sierra-de-los-porrones-la-pedriza-desde-parking-principal-p-n-de-guadarrama-16147016')">
+            <img src="https://s2.wklcdn.com/image_34/1027315/23183804/14727692Master.jpg" class="my-3" onclick="showAlertAndOpenNewTab('https://es.wikiloc.com/rutas-senderismo/ruta-del-agua-de-bunol-cueva-del-turche-charco-de-las-palomas-charco-manan-cueva-de-las-palomas-y-t-23183804')">
+            <img src="https://s1.wklcdn.com/image_87/2628884/24930448/15907162Master.jpg" class="my-3" onclick="showAlertAndOpenNewTab('https://es.wikiloc.com/rutas-senderismo/ruta-de-las-tres-cascadas-ardones-clotet-mascarada-cerler-24930446')">
         </div>
     </div>
     <div class="container text-center my-3">
         <div class="row row-cols-2">
-            <img src="https://s2.wklcdn.com/image_10/321744/17048094/10717259Master.jpg" onclick="window.location.href='https://es.wikiloc.com/rutas-senderismo/al-montcudio-520-m-y-la-mola-566-m-desde-parking-de-la-font-del-lor-17048074'">
-            <img src="https://s2.wklcdn.com/image_13/417894/6151701/3369029Master.jpg" onclick="window.location.href='https://es.wikiloc.com/rutas-senderismo/s-lorenzo-del-escorial-guarda-abantos-san-juan-naranjera-s-l-escorial-6151701'">
-            <img src="https://s0.wklcdn.com/image_19/590908/10240629/6156864Master.jpg" class="my-3" onclick="window.location.href='https://es.wikiloc.com/rutas-senderismo/silla-del-rey-cascada-de-la-chorranca-cueva-del-monje-y-cerro-del-puerco-valsain-10240623'">
-            <img src="https://s0.wklcdn.com/image_29/877835/14369933/8916867Master.jpg" class="my-3" onclick="window.location.href='https://es.wikiloc.com/rutas-senderismo/camino-santiago-frances-roncesvalles-santiago-de-compostela-gr-65-link-epilogo-finisterre-y-muxia-14369933'">
+            <img src="https://s2.wklcdn.com/image_10/321744/17048094/10717259Master.jpg" onclick="showAlertAndOpenNewTab('https://es.wikiloc.com/rutas-senderismo/al-montcudio-520-m-y-la-mola-566-m-desde-parking-de-la-font-del-lor-17048074')">
+            <img src="https://s2.wklcdn.com/image_13/417894/6151701/3369029Master.jpg" onclick="showAlertAndOpenNewTab('https://es.wikiloc.com/rutas-senderismo/s-lorenzo-del-escorial-guarda-abantos-san-juan-naranjera-s-l-escorial-6151701')">
+            <img src="https://s0.wklcdn.com/image_19/590908/10240629/6156864Master.jpg" class="my-3" onclick="showAlertAndOpenNewTab('https://es.wikiloc.com/rutas-senderismo/silla-del-rey-cascada-de-la-chorranca-cueva-del-monje-y-cerro-del-puerco-valsain-10240623')">
+            <img src="https://s0.wklcdn.com/image_29/877835/14369933/8916867Master.jpg" class="my-3" onclick="showAlertAndOpenNewTab('https://es.wikiloc.com/rutas-senderismo/camino-santiago-frances-roncesvalles-santiago-de-compostela-gr-65-link-epilogo-finisterre-y-muxia-14369933')">
         </div>
     </div>
 </section>
