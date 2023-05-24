7615e967f0b18b194ae5740b97b12a8498be1da3
Pablo-Hidalgo-Prados <pablohidalgo3@gmail.com>
2023-05-24 17:44:46 +0200

    Categorías y descuentos arreglados

--- Laravel/project-3-2/resources/views/auth/categorias/visualizec.blade.php
+++ Laravel/project-3-2/resources/views/auth/categorias/visualizec.blade.php
@@ -2,14 +2,15 @@
 
 @section('content')
 <html lang="{{ app()->getLocale() }}">
-<div class="w-50 border rounded p-3 mx-auto">
+<div class="container">
+    <div class="product-card border-success border-opacity-50 mt-5">
         <h3 class="text-center mb-4">@lang('messages.category_info_1') {{ $categoria->nombre }}</h3>
         <p><strong>@lang('messages.category_info_2')</strong> {{ $categoria->nombre }}</p>
         <p><strong>@lang('messages.category_info_3')</strong> {{ $categoria->descripcion }}</p>
-            <form action="{{ route('categorias.listar') }}" method="POST">
-                @csrf
-                <button class="btn btn-success" type="submit">@lang('messages.category_info_4')</button>
-            </form>
-        </div>
+        <form action="{{ route('categorias.listar') }}" method="POST">
+            @csrf
+            <button class="btn btn-success" type="submit">@lang('messages.category_info_4')</button>
+        </form>
+    </div>
 </div>
 @endsection
--- Laravel/project-3-2/resources/views/auth/descuentos/visualized.blade.php
+++ Laravel/project-3-2/resources/views/auth/descuentos/visualized.blade.php
@@ -2,14 +2,15 @@
 
 @section('content')
 <html lang="{{ app()->getLocale() }}">
-<div class="w-50 border rounded p-3 mx-auto">
+<div class="container">
+    <div class="product-card border-success border-opacity-50 mt-5">
         <h3 class="text-center mb-4">@lang('messages.discount_info_1') {{ $descuento->codigo }}</h3>
         <p><strong>@lang('messages.discount_info_2')</strong> {{ $descuento->codigo }}</p>
         <p><strong>@lang('messages.discount_info_3')</strong> {{ $descuento->porcentaje }}</p>
-            <form action="{{ route('descuentos.listar') }}" method="POST">
-                @csrf
-                <button class="btn btn-success" type="submit">@lang('messages.discount_info_4')</button>
-            </form>
-        </div>
+        <form action="{{ route('descuentos.listar') }}" method="POST">
+            @csrf
+            <button class="btn btn-success" type="submit">@lang('messages.discount_info_4')</button>
+        </form>
+    </div>
 </div>
 @endsection
